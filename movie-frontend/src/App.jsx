270965bdff25639f61f9e770c8b603543e679f2e
Iris Randolph <irandolph@hmc.edu>
2026-08-23 19:09:44 -0700

    basic component structure and conditional rendering

--- movie-frontend/src/App.jsx
+++ movie-frontend/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // conditional rendering
+  const movieNumber = 2;
 
   return (
-    <div></div>
+    <>
+      {movieNumber === 1 && <MovieCard movie={{title: "Placeholder Title", release_date: "2024", }} />}
+      {movieNumber === 2 && <MovieCard movie={{title: "Sharknado 12", release_date: "2054", }} />}
+    </>
   )
 }
 
